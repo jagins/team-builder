9d948ceb05f1869f52c33ca8d1c4f9be6c7fa7a4
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-18 15:35:20 -0600

    setup handleChange and submitForm()s with implementations

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,50 @@
 import React, { useState } from 'react';
 
-function Form() {
+function Form(props) {
     const [teamMember, setTeamMember] = useState({
         name: '',
         email: '',
         role: ''
     });
 
-    const handleChange = event => {
-
+    const handleChange = event => 
+    {
+        setTeamMember({...teamMember, [event.target.id]: event.target.value});
     }
 
-    const submitForm = event => {
-
+    const submitForm = event => 
+    {
+        event.preventDefault();
+        props.addNewTeamMember(teamMember);
+        setTeamMember({
+            name: '',
+            email: '',
+            role: ''
+        });
     }
     return (
         <form onSubmit={submitForm}>
             <label htmlFor='name'>Name</label>
-            <input id='name'
+            <input 
+                id='name'
                 type='text'
                 onChange={handleChange}
                 value={teamMember.name} />
+           
             <label htmlFor='email'>email</label>
-            <input id='email'
+            <input 
+                id='email'
                 type='email'
                 onChange={handleChange}
                 value={teamMember.name} />
+           
             <label htmlFor='role'>Role</label>
-            <input id='role'
+            <input 
+                id='role'
                 type='text'
                 onChange={handleChange}
                 value={teamMember.name} />
+            
             <button type='submit'>Add Team Member</button>
         </form>
     );
