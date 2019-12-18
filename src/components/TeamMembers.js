c260c169545aba019b74a229d49300ad98f80f97
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-18 15:24:22 -0600

    added in skeleton for team-member card

---
+++ src/components/TeamMembers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TeamMembers(props)
+{
+    return(
+    <div className='team-list'>
+        <div className='team-member' key=''>
+            <h2>Name</h2>
+            <p>Email</p>
+            <p>Role</p>
+        </div>
+    </div>
+    );
+}
+
+export default TeamMembers;
